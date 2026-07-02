f9900bdebff1d96f1656ceb9769a5e20ceb8a2aa
s6906022610193-cyber <s6906022610193@email.kmutnb.ac.th>
2026-07-02 11:00:48 +0700

    test

---
+++ Lecture-01/testStrNum.py
@@ -0,0 +1,4 @@
+print ("My age is ",25, "I have", 3500.50,"Bth.")
+print ("My age is %d I have %.2f Bth." %(25, 3500.50))
+print ("My age is "+ str (25)+" I have " + str (3500.50)+" Bth.") #เชื่อมคำ เปลี่ยน Num เป็น Str +(25)
+print ("My age is {0} I have {1:.2f} Bth.".format(25,3500.50)) #เรียกใช้ format method
